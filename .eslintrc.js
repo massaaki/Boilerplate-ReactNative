6a9fda8d0de2c7808c7a8fd4f32833c608cc005e
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-05-09 23:20:04 -0300

    configure estlint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,40 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+  },
+  extends: [
+    // 'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'react/prefer-stateless-function': 'off',
+    'react/state-in-constructor': 'off',
+    'react/no-unused-state': 'off',
+    'react/static-property-placement': 'off',
+    'react/react/require-default-props': 'off',
+  },
 };
